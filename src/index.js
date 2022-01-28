cdad88c916633d9e56f58042e25adee15517ded4
IvoMart <ivo_ivancho@hotmail.com>
2022-01-28 15:26:15 -0300

    Adding styles files: scss -> css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { JournalApp } from './components/JournalApp';
+import './styles/styles.css'
 
 ReactDOM.render(
   <JournalApp/>,
