0ef115c8cba50c463506d2cce38cafb6e0d73247
IvoMart <ivo_ivancho@hotmail.com>
2022-05-10 19:48:56 -0300

    authReducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,38 @@
+import { types } from "../types/types";
+
+/**
+ * 
+ * {
+ *  uid: 'asljkf2150235',
+ *  name: 'Usuario',
+ *  dir:{
+ *      calle: 12,
+ *      altura: 15
+ *  }
+ * }
+ * 
+ */
+
+
+const initialState = {
+    uid: 'asljkf2150235',
+    name: 'Usuario',
+    dir: {
+        calle: 12,
+        altura: 15
+    }
+};
+
+export const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                uid: action.payload.id,
+                name: action.payload.displayName
+            };
+        case types.logout:
+            return {};
+        default:
+            return state;
+    }
+}
