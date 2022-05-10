3bf11e342d54fd52c06036770bfffe379a9a1ae9
IvoMart <ivo_ivancho@hotmail.com>
2022-05-10 19:48:25 -0300

    devTools: Cambios de Store - @reduxjs/toolkit

---
+++ src/store/Store.js
@@ -0,0 +1,28 @@
+// import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+import { authReducer } from '../reducers/authReducer';
+
+
+
+/*
+const reducers = combineReducers({
+    auth: authReducer
+});
+
+export const store = createStore(
+    reducers, +
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+*/
+
+
+const reducer = {
+    auth: authReducer
+}
+
+export const store = configureStore({
+    reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    // devTools: process.env.NODE_ENV !== 'production'
+})
